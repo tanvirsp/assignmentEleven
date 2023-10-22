0aa1e6a2fd9725065cb5f96ab941c2ce199a5d5b
tanvirsp <tanvirgraphic@gmail.com>
2023-10-23 00:40:16 +0600

    Assignment Done

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
         <section className="project-section">
             <div className="container">
                 <div className="title">
-                    <h2>This is Project Title</h2>
+                    <h2>This is Project Section</h2>
                     <p>
                         To continue making yoga accessible to everyone we offer classes
                         online, in-person at our studio (mask required), and outdoors.
